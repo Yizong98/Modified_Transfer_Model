7ad226d6515f1c6ea6f679d6cf3cbcc066b30236
Yizong98 <yizong@ucsd.edu>
2018-06-10 22:47:19 -0700

    feat: Upload new transfer model

---
+++ example_feat_extract.py
@@ -0,0 +1,73 @@
+from keras.layers import Input, Dense
+from keras.models import Model
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers.convolutional import Convolution2D
+from keras.layers.convolutional import MaxPooling2D, ZeroPadding2D
+from keras.layers.normalization import BatchNormalization
+from keras.layers.advanced_activations import ELU
+from keras.utils.data_utils import get_file
+from keras.layers import Input, Dense
+import time
+import numpy as np
+from keras import backend as K
+import audio_processor as ap
+import pdb
+from music_tagger_cnn import MusicTaggerCNN
+from music_tagger_crnn import MusicTaggerCRNN
+
+
+def librosa_exists():
+    try:
+        __import__('librosa')
+    except ImportError:
+        return False
+    else:
+        return True
+
+
+def main(net):
+    ''' *WARNIING*
+    This model use Batch Normalization, so the prediction
+    is affected by batch. Use multiple, different data 
+    samples together (at least 4) for reliable prediction.'''
+
+    print('Running main() with network: %s and backend: %s' % (net, K._BACKEND))
+    # setting
+    audio_paths = ['data/bensound-cute.mp3',
+                   'data/bensound-actionable.mp3',
+                   'data/bensound-dubstep.mp3',
+                   'data/bensound-thejazzpiano.mp3']
+    melgram_paths = ['data/bensound-cute.npy',
+                     'data/bensound-actionable.npy',
+                     'data/bensound-dubstep.npy',
+                     'data/bensound-thejazzpiano.npy']
+
+    # prepare data like this
+    melgrams = np.zeros((0, 1, 96, 1366))
+
+    if librosa_exists:
+        for audio_path in audio_paths:
+            melgram = ap.compute_melgram(audio_path)
+            melgrams = np.concatenate((melgrams, melgram), axis=0)
+    else:
+        for melgram_path in melgram_paths:
+            melgram = np.load(melgram_path)
+            melgrams = np.concatenate((melgrams, melgram), axis=0)
+
+    # load model like this
+    if net == 'cnn':
+        model = MusicTaggerCNN(weights='msd', include_top=False)
+    elif net == 'crnn':
+        model = MusicTaggerCRNN(weights='msd', include_top=False)
+    # predict the tags like this
+    print('Predicting features...')
+    start = time.time()
+    features = model.predict(melgrams)
+    print (features[:, :10])
+    return
+
+if __name__ == '__main__':
+
+    networks = ['cnn', 'crnn']
+    for net in networks:
+        main(net)
